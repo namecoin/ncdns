d2e0c4ce38ed8e6ff888c26bbd22675639d6e515
Jeremy Rand <jeremyrand@danwin1210.de>
2022-12-28 04:08:10 +0000

    Cirrus: Add README/license/docs to Nightly artifacts

--- testdata/dist.bash
+++ testdata/dist.bash
@@ -8,6 +8,10 @@ mkdir dist
 pushd idist || exit 1
 for x in *; do
   echo "$x"
+  mkdir -p "$x/doc"
+  cp -a "../README.md" "$x/"
+  cp -a "../COPYING" "$x/"
+  cp -a "../_doc/"* "$x/doc/"
   tar -caf "../dist/$(basename "$x").tar.gz" "$x"
 done
 popd || exit 1
