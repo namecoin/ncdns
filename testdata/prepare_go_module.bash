577ddafd84b44a6ba587c14e1f1307ee4665911e
Jeremy Rand <jeremyrand@danwin1210.de>
2026-09-05 14:31:13 +0000

    CI: Remove version from systemd_path
    Seems gccgo doesn't like it.

--- testdata/prepare_go_module.bash
+++ testdata/prepare_go_module.bash
@@ -5,7 +5,7 @@ set -euxo pipefail
 cp testdata/go.mod testdata/go.sum .
 
 go mod download github.com/coreos/go-systemd/v22
-systemd_path="$(go list -m -f '{{.Dir}}' github.com/coreos/go-systemd/v22)"
+systemd_path="$(go list -m -f '{{.Dir}}' github.com/coreos/go-systemd)"
 go mod edit -replace "github.com/coreos/go-systemd=$systemd_path"
 
 # Temporarily disabled because it's not supported in gccgo yet (was added in Go 1.23).
